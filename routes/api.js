fd1fbd289f3fc36ddbabf74ae9a5e4b245c3ddd6
Algorithm <alexlo0612@outlook.com>
2019-01-12 12:45:51 +0100

    update

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ const express = require("express");
 const request = require("request");
 const router = express.Router();
 const bodyParser = require("body-parser");
+const rp = require("request-promise");
 
 //Set Global Vars.
 const transcript_url = "https://shimasu.herokuapp.com/api/get_transcript/k3vFX0YrkMY";
@@ -31,37 +32,76 @@ app.use(
 // });
 
 //Make transcript API Call - For In Loop - Faster
-request(encodeURI(transcript_url), (err, response, body) => {
-    if (err) {
-        console.error(err);
-    } else {
-        const stage1 = JSON.parse(body);
-        const start_val_array = [];
-        for (let x in stage1) {
-            start_val_array.push(`${stage1[x].text}`);
+
+function transcript_api(x) {
+    request(encodeURI(x), (err, response, body) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const stage1 = JSON.parse(body);
+            const text_val_array = [];
+            for (let x in stage1) {
+                text_val_array.push(`${stage1[x].text}`);
+            };
         };
-        //Make suggestion API Call
-        request(encodeURI(suggestion_url), (err, response, body) => {
-            if (err) {
-                console.error(err);
-            } else {
-                const stage2 = JSON.parse(body);
-                const name_val_array = [];
-                for (let x in stage2) {
-                    name_val_array.push(`${stage2[x].name}`);
-                };
-                //Operation Take Place Here
-                const r1 = new Set(name_val_array);
-                const r2 = new Set(start_val_array);
-            }
-        });
+    });
+};
 
+
+//Make suggestion API Call
+// function suggestion_api(x) {
+//     request(encodeURI(x), (err, response, body) => {
+//         if (err) {
+//             return callback(err);
+//         } else {
+//             const stage2 = JSON.parse(body);
+//             const name_val_array = [];
+//             for (let x in stage2) {
+//                 name_val_array.push(`${stage2[x].name}`);
+//             };
+//         };
+//     });
+// };
+
+//sss
+
+request(encodeURI(suggestion_url), (err, response, body) => {
+    const stage2 = JSON.parse(body);
+    const name_val_array = [];
+    for (let x in stage2) {
+        name_val_array.push(`${stage2[x].name}`);
     };
-
 });
+console.log(name_val_array);
+//Callback Demo
+// function req(x) {
+//     request(encodeURI(x), (err, response, body) => {
+//         const name_vale_array = [];
+//         for (let x in JSON.parse(body)) {
+//             name_vale_array.push(JSON.parse(body)[x].name);
+//         };
+//         console.log(name_vale_array);
+//     });
+// };
+
+// function sayHi(x) {
+//     console.log(x);
+// };
+// sayHi(req(suggestion_url));
 
 
-
+//Callback --Listing
+function list(a, b) {
+    const r1 = [];
+    const r2 = [];
+    for (let x in a) {
+        r1.push(a[x]);
+    };
+    for (let x in b) {
+        r2.push(b[x]);
+    };
+    return r1, r2;
+};
 
 
 
